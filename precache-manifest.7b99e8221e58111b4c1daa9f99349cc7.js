1be5235a123eacd99ae01008e5fbd007e43d62bf
fbuitron <franciscobuitron21@gmail.com>
2020-04-13 22:52:59 -0700

    Updates

---
+++ precache-manifest.7b99e8221e58111b4c1daa9f99349cc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0828c5405c1a189679ddc780b2c074c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0426382488760a92239",
+    "url": "/static/css/main.32410464.chunk.css"
+  },
+  {
+    "revision": "36d40cd84e351c20fbf4",
+    "url": "/static/js/2.34e28ab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.34e28ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0426382488760a92239",
+    "url": "/static/js/main.bbbd79c0.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "ba701aa6890ef9e2a9115d89cf9ac491",
+    "url": "/static/media/About-me.ba701aa6.jpg"
+  },
+  {
+    "revision": "e13e10b8c38ce9ffbc7e68dee212c0d0",
+    "url": "/static/media/Pokebattles-logo.e13e10b8.png"
+  },
+  {
+    "revision": "4af5e02b2576b681987cec92b66dcc73",
+    "url": "/static/media/eatvent-logo.4af5e02b.png"
+  }
+]);
